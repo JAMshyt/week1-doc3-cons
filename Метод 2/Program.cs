372cef6c3398ca760d08c09922ea36408d53507a
JAMshyt <77636014+JAMshyt@users.noreply.github.com>
2022-12-05 16:47:19 +0300

    Add files via upload

---
+++ Метод 2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Метод_2
+{
+    class Program
+    {
+        static double f(double x, out double y)
+        {
+            if ((x + 2) <= 1) y = x * x;
+            else if (1 < (x + 2) & (x + 2) < 10) y = 1 / (x + 2);
+            else y = x + 2;
+            return y;
+        }
+
+        static double f(double x)
+        {
+            double y;
+            if ((x + 2) <= 1) y = x * x;
+            else if (1 < (x + 2) & (x + 2) < 10) y = 1 / (x + 2);
+            else y = x + 2;
+            return y;
+
+        }
+        static void Main(string[] args)
+        {
+            double a, b, h;
+            do
+            {
+                try
+                {
+                    do
+                    {
+                        Console.Write("a=");
+                        a = double.Parse(Console.ReadLine());
+                        Console.Write("b=");
+                        b = double.Parse(Console.ReadLine());
+                        Console.Write("h=");
+                        h = double.Parse(Console.ReadLine());
+                        if (a < b) break;
+                        else Console.WriteLine("а Должно быть меньше б");
+
+                    } while (true);
+
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка, заново");
+                }
+            } while (true);
+            for (double i = a; i <= b; i += h)
+            {
+                Console.WriteLine("f({0:f2})={1:f4}", i, f(i));
+            }
+
+            Console.WriteLine("\n");
+
+            for (double i = a; i <= b; i += h)
+            {
+                double y;
+                Console.WriteLine("f({0:f2})={1:f4}", i, f(i,out y));
+            }
+        }
+    }
+}
---
+++ Методы 1 К/Методы 1 К/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Методы_1_К
+{
+    internal class Program
+    {
+        static void f(double x)
+        {
+            if (x % 5 == 0)
+            {
+                Console.WriteLine("Результат (кратное): " + x / 5);
+            }
+            else if ((x *10)%5 == 0)
+            {
+                Console.WriteLine("Результат (кратное): " + x / 5);
+            }
+            else
+            {
+                Console.WriteLine("Результат (не кратное):" + (++x));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            double a;
+            do
+            {
+                try
+                {
+                    Console.Write("Введите число: ");
+                    a = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Еррор");
+                }
+            } while (true);
+            f(a);
+        }
+    }
+}
